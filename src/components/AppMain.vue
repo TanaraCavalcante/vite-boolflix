ae588c57436b5e1ba7d731e7420b6d7071a70478
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-09-20 19:22:40 -0400

    API

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,12 +6,34 @@ import axios from 'axios';
   export default {
      data() {
         return {
-          moviesList:[]
+          moviesList:[],
+          apiUrl:"",
         }
   },
   components:{
     SearchBar,
     MovieList
+  },
+  methods:{
+    getMoviesList(){
+      console.log("Chiamata API iniziata!");
+      axios.get(this.apiUrl)
+        .then((response) => {
+           // handle success
+           console.log(response);
+        })
+       .catch(function (error) {
+          // handle error
+           console.log(error);
+        })
+        .finally(function () {
+          // always executed
+          console.log("Chiamata API terminata!")
+        });
+      }
+  },
+  created(){
+      this.getMoviesList()
   }
 }
 </script>
